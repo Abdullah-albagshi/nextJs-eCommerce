faea04adad15fc5ada353f855ee522fd975b0514
Abdullah albagshi <abdullah.albagshi@gmail.com>
2024-01-31 10:48:35 +0300

    Add Navbar and Footer components

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer className='container mx-auto'>
+			<div className=''>footer</div>
+		</footer>
+	);
+};
+
+export default Footer;
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,62 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='container mx-auto h-20 py-[.625rem] px-4 flex justify-between items-center sticky top-0 bg-white'>
+      <nav>
+        <ul className='flex'>
+          <li className='py-4 px-3'>
+            <Link href='/'>
+              Shop
+            </Link>
+          </li>
+          <li className='py-4 px-3'>
+            <Link href='/product/lamp'>
+              Categories
+            </Link>
+          </li>
+          <li className='py-4 px-3'>
+            <Link href='/'>
+              Pages
+            </Link>
+          </li>
+          <li className='py-4 px-3'>
+            <Link href='/'>
+              Elements
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div id='logo' className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+        <Link href='/'>
+          <img src="https://savoy.nordicmade.com/wp-content/themes/savoy/assets/img/logo@2x.png" className='h-4' alt="logo" />
+        </Link>
+      </div>
+      <nav className="">
+        {/* whitelist, sign in, cart */}
+        <ul className='flex'>
+          <li className='py-4 px-3'>
+            <Link href='/'>
+              Wishlist
+            </Link>
+          </li>
+          <li className='py-4 px-3'>
+            <Link href='/'>
+              Sign in
+            </Link>
+          </li>
+          <li className='py-4 px-3'>
+            <Link href='/'>
+              Cart
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+    </header>
+  )
+}
+
+export default Navbar
