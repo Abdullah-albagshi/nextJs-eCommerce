3343919ee0658b1cb87608b2c2dd0e1c93130c03
Abdullah albagshi <abdullah.albagshi@gmail.com>
2024-02-03 21:56:11 +0300

    Refactor loading component layout

--- app/product/[productName]/loading.tsx
+++ app/product/[productName]/loading.tsx
@@ -4,9 +4,9 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 const Loading = () => {
   return (
-    <div className="flex-1 flex mx-auto basis-full gap-4 justify-center pt-4 bg-ecm-gray-lightest">
-      <Skeleton className='h-[600px] w-1/3 bg-gray-400' />
-      <Skeleton className='h-[600px] w-1/3 bg-gray-400' />
+    <div className="flex flex-col justify-center flex-1 gap-4 pt-4 mx-auto md:flex-row basis-full bg-ecm-gray-lightest">
+      <Skeleton className='h-[600px] md:w-1/3 bg-gray-400' />
+      <Skeleton className='h-[600px] md:w-1/3 bg-gray-400' />
 
     </div>
   )
