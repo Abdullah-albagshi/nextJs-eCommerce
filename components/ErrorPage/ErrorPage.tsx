849363431735729e7500b53749d3d1f143c31bee
Abdullah albagshi <abdullah.albagshi@gmail.com>
2024-02-04 09:22:21 +0300

    Update ErrorPage component layout and content

--- components/ErrorPage/ErrorPage.tsx
+++ components/ErrorPage/ErrorPage.tsx
@@ -10,7 +10,7 @@ const ErrorPage = () => {
       className={cn(
         'mx-auto ecm-max-width',
         'flex flex-col items-center',
-        'flex-1 justify-center w-full '
+        ' justify-start w-full mt-10 gap-2'
       )}
     >
       <X
@@ -18,10 +18,13 @@ const ErrorPage = () => {
         strokeWidth={2}
         className='p-2 rounded-full bg-ecm-gray animate-pulse'
       />
-      <p className={cn('text-ecm-black')}>Page not found.</p>
+      <h2 className={cn('text-ecm-black font-light text-2xl')}>Page not found.</h2>
+      <p className={cn('text-ecm-gray max-w-sm text-balance text-center')}>It looks like nothing was found at this location.</p>
+      <p className={cn('text-ecm-gray max-w-sm text-balance text-center')}>Click the link below to return home.</p>
 
-      <Link href={'/'} className='ecm-black-button' >
-        <ArrowLeft size={16} strokeWidth={2} className='mr-2' />Go Home
+      <Link href={'/'} className='ecm-black-button !w-fit flex items-center !p-2' >
+        <ArrowLeft size={16} strokeWidth={2} className='mr-2' />
+        <span>Go Home</span>
       </Link>
     </div>
   )
