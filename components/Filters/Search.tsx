a86b441ffddadd16737b4c07d9ef3914e72408d4
Abdullah albagshi <abdullah.albagshi@gmail.com>
2024-02-04 11:17:58 +0300

    Refactor search functionality and add search result message

--- components/Filters/Search.tsx
+++ components/Filters/Search.tsx
@@ -1,9 +1,10 @@
 'use client';
 
+import { useRouter, useSearchParams } from 'next/navigation';
+
 import React from 'react';
 import { X } from 'lucide-react';
 import { cn } from '@/lib/utils';
-import { useRouter } from 'next/navigation';
 
 type Props = {
 	isSearchToggled: boolean;
@@ -17,14 +18,18 @@ const Search = ({
 	...props
 }: Props) => {
 	const router = useRouter();
-	const [search, setSearch] = React.useState('');
-
+  // read the query from the URL
+	const searchParams = useSearchParams();
+	const query = searchParams.get('q');
+  // when the form is submitted, we update the URL with the search query
 	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		if (!e.currentTarget) return;
+		const search = e.currentTarget.querySelector('input')?.value || '';
 		e.preventDefault();
 		router.push(`/?q=${search}`);
 	};
-	const clearSearch = () => {
-		setSearch('');
+  // clear the search query from the URL
+	const closeSearch = () => {
 		router.push('/');
 		setIsSearchToggled(false);
 	};
@@ -33,7 +38,7 @@ const Search = ({
 		<div
 			className={cn(
 				'animate-fade-in bg-white p-3 h-0 transition-all  ease-in-out ',
-				isSearchToggled ? 'h-32' : 'h-0',
+				isSearchToggled ? 'h-40' : 'h-0',
 				className
 			)}
 			{...props}
@@ -48,15 +53,13 @@ const Search = ({
 							'placeholder-text-ecm-gray',
 							isSearchToggled ? 'block' : 'hidden'
 						)}
-						value={search}
-						onChange={(e) => setSearch(e.target.value)}
 					/>
 					<span
 						className={cn(
 							'absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer',
 							isSearchToggled ? 'block' : 'hidden'
 						)}
-						onClick={clearSearch}
+						onClick={closeSearch}
 					>
 						<X size={24} strokeWidth={2} />
 					</span>
@@ -65,7 +68,7 @@ const Search = ({
 				<p
 					className={cn(
 						'text-ecm-gray text-sm p-3 h-0 animate-fade-in transition-all duration-200 ease-in-out',
-						search.length === 0 ? 'hidden' : 'block h-10'
+						!isSearchToggled ? 'hidden' : 'block h-10'
 					)}
 				>
 					press{' '}
@@ -73,6 +76,14 @@ const Search = ({
 						<u> Enter</u>{' '}
 					</span>{' '}
 					to search.
+				</p>
+				<p
+					className={cn(
+						'h-0 p-3 text-sm transition-all duration-200 ease-in-out text-ecm-gray animate-fade-in',
+						(query && isSearchToggled) ? 'block h-10' : 'hidden'
+					)}
+				>
+					{query ? `Search results for "${query}"` : 'No search results'}
 				</p>
 			</form>
 		</div>
