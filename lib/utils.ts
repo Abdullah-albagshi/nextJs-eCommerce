a284dda2e77910ca7bb4714725018ef304d2cef2
Abdullah albagshi <abdullah.albagshi@gmail.com>
2024-02-04 09:55:40 +0300

    Add setCookie and getCookie functions to utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,12 +6,13 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 // Helper functions for working with cookies
+// it will set a cookie with the given name and value and expiration days
 export const setCookie = (name: string, value: string, days: number) => {
   const expires = new Date();
   expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
   document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
 }
-
+// it will return the value of the cookie with the given name
 export const getCookie = (name: string) => {
   const cookieName = `${name}=`;
   const decodedCookie = decodeURIComponent(document.cookie);
